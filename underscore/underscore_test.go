bc7d0b9b350381fea08155d690cecefe1532f6e8
dmcclung <35938+dmcclung@users.noreply.github.com>
2023-12-24 22:31:02 -0600

    map for test cases

--- underscore/underscore_test.go
+++ underscore/underscore_test.go
@@ -1,33 +1,24 @@
 package underscore_test
 
-import (
-	"fmt"
+import (	
 	"testing"
 
 	"github.com/dmcclung/twg/underscore"
 )
 
 func TestCamel(t *testing.T) {
-	testCases := []struct {
+	testCases := map[string]struct {
 			arg string
 			want string
 		}{
-			{
-				"thisIsCamelCase", "this_is_camel_case",
-			},
-			{
-				"aFuncZ", "a_func_z",
-			},		
-			{
-				"alllowercase", "alllowercase",
-			},
-			{
-				"snake_case_already", "snake_case_already",
-			},
+			"camel case": { "thisIsCamelCase", "this_is_camel_case" },
+			"two capitals": { "aFZ", "a_f_z" },
+			"all lower case": { "alllowercase", "alllowercase" },
+			"snake case": { "snake_case_already", "snake_case_already" },
 		}
 
-	for _, tc := range testCases {
-		t.Run(fmt.Sprintf(tc.arg), func(t *testing.T) {
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
 			if got := underscore.Camel(tc.arg); got != tc.want {
 				t.Errorf("Camel(%s), want %s, got %s", tc.arg, tc.want, got)
 			}
