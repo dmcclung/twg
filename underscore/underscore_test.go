703b5c5dbb4379b49865090abab4fdf6014cb02a
dmcclung <35938+dmcclung@users.noreply.github.com>
2023-12-25 22:42:25 -0600

    formatting

--- underscore/underscore_test.go
+++ underscore/underscore_test.go
@@ -1,6 +1,6 @@
 package underscore_test
 
-import (	
+import (
 	"testing"
 
 	"github.com/dmcclung/twg/underscore"
@@ -8,14 +8,14 @@ import (
 
 func TestCamel(t *testing.T) {
 	testCases := map[string]struct {
-			arg string
-			want string
-		}{
-			"camel case": { "thisIsCamelCase", "this_is_camel_case" },
-			"two capitals": { "aFZ", "a_f_z" },
-			"all lower case": { "alllowercase", "alllowercase" },
-			"snake case": { "snake_case_already", "snake_case_already" },
-		}
+		arg  string
+		want string
+	}{
+		"camel case":     {"thisIsCamelCase", "this_is_camel_case"},
+		"two capitals":   {"aFZ", "a_f_z"},
+		"all lower case": {"alllowercase", "alllowercase"},
+		"snake case":     {"snake_case_already", "snake_case_already"},
+	}
 
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
